b2ad0321ceba9d51e20194ca2158acc42dce9b73
Kakkoo <58194538+Kakkoo@users.noreply.github.com>
2020-08-08 14:43:17 -0700

    interface

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -1,13 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ICurrentWeather } from '../icurrent-weather';
 @Component({
   selector: 'app-current-weather',
   templateUrl: './current-weather.component.html',
   styleUrls: ['./current-weather.component.css']
 })
 export class CurrentWeatherComponent implements OnInit {
+current: ICurrentWeather
+  constructor() {
 
-  constructor() { }
+    this.current = {
+      city: 'Redmond',
+      country: 'US',
+      date: new Date(),
+      image: '',
+      temperature: 59,
+      description: 'rainy'
+    }
+  }
 
   ngOnInit(): void {
   }
